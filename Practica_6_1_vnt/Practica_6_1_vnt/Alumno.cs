23c3255f143aeae8aa0fd8f922c67625f6943995
victorNT <victornogueraterol@gmail.com>
2019-04-14 19:16:17 +0200

    Cambios con Alumno2

---
+++ Practica_6_1_vnt/Practica_6_1_vnt/Alumno.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Practica_6_1_vnt
+{
+    public class Alumno
+    {
+        private string nombre;
+        private int nota;
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+        public int Nota
+        {
+            get { return nota; }
+            set
+            {
+                if (value >= 0 && value <= 10)
+                    nota = value;
+            }
+        }
+        public Boolean Aprobado
+        {
+            get
+            {
+                if (nota >= 5)
+                    return true;
+                else
+                    return false;
+            }
+        }
+    }
+}
